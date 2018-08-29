72f3af92a297182950c646b3903833d6411a0976
Glitch (twisty-colon) <none>
2018-08-29 18:58:40 +0000

    💂🎏 Updated with Glitch

---
+++ app/components/drumPad.jsx
@@ -0,0 +1,55 @@
+const React = require('react');
+
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    this.clickMakeSound = this.clickMakeSound.bind(this);
+    this.handleKey = this.handleKey.bind(this);
+  
+  }
+  
+  clickMakeSound(soundId) {
+    if(this.props.powerSwitch === true){
+        let targetSound = document.getElementById(soundId)
+        targetSound.currentTime = 0;
+        targetSound.play(); 
+    }
+  }
+  
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKey);
+  }
+  
+  handleKey(e) {
+    let result = this.props.soundStore.filter(sound => {
+      return sound.keyCode === e.keyCode
+    })
+    this.clickMakeSound(result[0].keyTrigger)
+  }
+    
+  render() {
+     return (
+       <div id="drum-buttons"> 
+         {
+           this.props.soundStore.map((sound, i) => 
+               <button id={sound.id} onKeyDown={this.handleKey} className="drum-pad" key={i} onClick={this.clickMakeSound.bind(this, sound.keyTrigger)}>
+                 <audio id={sound.keyTrigger}  src={sound.url} autostart="false"></audio>
+                <span>{sound.keyTrigger}</span>
+                <hr />
+                {sound.id}
+                  
+               </button>)
+         }
+       </div>
+      );
+  }
+ 
+}
+
+module.exports = DrumPad;
